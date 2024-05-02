efe40a480fa44a1418050f1b23a5f91e86511f4c
J3TLA9 <johnmichael.relova118@gmail.com>
2024-05-02 22:51:43 +0800

    Adding difficulty

--- gclient.py
+++ gclient.py
@@ -8,21 +8,32 @@ port = 7777
 s = socket.socket()
 s.connect((host, port))
 
-# received the banner
-data = s.recv(1024)
-# print banner
-print(data.decode().strip())
 
 while True:
-    #let get our input from the user
-    user_input = input("").strip()
-
-    s.sendall(user_input.encode())
-    reply = s.recv(1024).decode().strip()
-    if "Correct" in reply:
-        print(reply)
+    # received the banner
+    data = s.recv(1024).decode().strip()
+    # print banner
+    print(data)
+    
+    if "Please enter your name:" in data:
+        #getting name
+        user_name = input("").strip()
+        s.sendall(user_name.encode())
+        continue
+        
+    if "Please enter a difficulty:" in data:
+        #getting difficulty
+        user_diff = input("").strip()
+        s.sendall(user_diff.encode())
+        continue
+        
+    if "Enter your guess:" in data: 
+        while not "Correct Answer!" in data
+            #let get our input from the user
+            user_input = input("").strip()
+            s.sendall(user_input.encode())
+            continue
         break
-    print(reply)
-    continue
+        
 s.close()
 
--- gserver.py
+++ gserver.py
@@ -3,7 +3,20 @@ import random
 
 host = "localhost"
 port = 7777
-banner = """
+
+intro_banner = """
+Welcome to the Guessing Game 1.0!
+Please enter your name: 
+"""
+
+diff_banner="""
+[]Easy
+[]Medium
+[]Hard
+Please enter a difficulty:
+"""
+
+guess_banner = """
 == Guessing Game v1.0 ==
 Enter your guess:"""
 
@@ -16,6 +29,7 @@ s.bind((host, port))
 s.listen(5)
 print(f"server address is {socket.gethostbyname(host)}")
 print(f"server is listening in port {port}")
+
 guessme = 0
 conn = None
 
@@ -23,26 +37,51 @@ while True:
     if conn is None:
         print("waiting for connection..")
         conn, addr = s.accept()
-        guessme = generate_random_int(1,100) # Change the amount upon the difficulty
         print(f"new client: {addr[0]}")
-        # cheat_str = f"==== number to guess is {guessme} \n" + banner 
-        # conn.sendall(cheat_str.encode())
-        conn.sendall(banner.encode())
-    else:
-        client_input = conn.recv(1024)
-        guess = int(client_input.decode().strip())
-        print(f"User guess attempt: {guess}")
-        if guess == guessme:
-            conn.sendall(b"Correct Answer!")
-            conn.close()
-            conn = None
-            continue
-        elif guess > guessme:
-            conn.sendall(b"Guess Lower!\nenter guess: ")
-            continue
-        elif guess < guessme:
-            conn.sendall(b"Guess Higher!\nenter guess:")
-            continue
+        
+    else:        
+        conn.sendall(intro_banner.encode())
+        client_name = conn.recv(1024).decode().strip().upper()
+        
+        while client_name:
+            #Setting the difficulty
+            conn.sendall(diff_banner.encode())
+            client_diff = conn.recv(1024).decode().strip().lower()
+            
+            if client_diff not in("easy", "medium", "hard"):
+                conn.sendall(b"Invalid difficulty choice. Please choose again.")
+                continue
+                
+            #Setting the values
+            low, high = {"easy":(1,50), "medium":(1,100), "hard":(1,500)}[client_diff] #Based on difficulty given
+            guessme = generate_random_int(low,high)
+            print(f"User difficulty choice: {client_diff}")
+            
+            #Setting client guess
+            conn.sendall(guess_banner.encode())
+            # cheat_str = f"==== number to guess is {guessme} \n" + banner 
+            # conn.sendall(cheat_str.encode())
+            
+            while True:
+                client_guess = conn.recv(1024)
+                try:
+                    guess = int(client_guess.decode().strip())
+                    print(f"User guess attempt: {guess}")
+                    
+                    if guess == guessme:
+                        conn.sendall(b"Correct Answer!")
+                        conn.close()
+                        conn = None
+                        continue
+                    elif guess > guessme:
+                        conn.sendall(b"Guess Lower!\nenter guess: ")
+                        continue
+                    elif guess < guessme:
+                        conn.sendall(b"Guess Higher!\nenter guess:")
+                        continue
+                        
+                except ValueError:
+                    conn.sendall(b"Invalid input. Pleace enter a number.")
 
 
 
